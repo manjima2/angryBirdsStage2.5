299229de9460941659b5c9543c5bdb4a3e672d45
manjima2 <84669830+manjima2@users.noreply.github.com>
2021-08-13 10:54:11 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,10 +4,10 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var box1, pig1;
-var backgroundImg,platform;
+var backgroundImage;
 
-function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+function preload(){
+  backgroundImage = loadImage("sprites/bg.png");
 }
 
 function setup(){
@@ -15,9 +15,8 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-
-    ground = new Ground(600,height,1200,20);
-    //platform = new Ground(150, 305, 300, 170);
+    
+    ground = new Ground(600,height,1200,20)
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -39,7 +38,7 @@ function setup(){
 }
 
 function draw(){
-    background(backgroundImg);
+    background(backgroundImage);
     Engine.update(engine);
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
@@ -60,5 +59,4 @@ function draw(){
     log5.display();
 
     bird.display();
-    //platform.display();
 }
